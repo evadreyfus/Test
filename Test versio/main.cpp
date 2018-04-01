eb74f3e40a44952f6e91ac612e4e88c28ab8652c
unknown <evadreyfus@hotmail.fr>
2018-04-01 17:17:11 +0200

    First Try

---
+++ Test versio/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    int z;
+    cout << "Rentrez un chiffre" << endl;
+    cin >> x;
+    cout << "Rentrez un deuxième chiffre"<<endl;
+    cin >> y;
+    z = x*y;
+    cout << "Le resultat de la multi est : " << z << endl;
+    return 0;
+}
